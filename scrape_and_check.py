72bd92df432e1a0d0ef48028405634c5dd958f63
rodcoelho <rodcpython@gmail.com>
2018-06-07 13:48:36 -0400

    found new solution for checking if page changed

--- scrape_and_check.py
+++ scrape_and_check.py
@@ -2,25 +2,26 @@
 
 import re
 from bs4 import BeautifulSoup
-from bs4.element import Comment
 from urllib.request import Request, urlopen
 import time
 from pprint import pprint
-
 
 # from send_messages import sendtext
 # from writeout import write_out
 
 
 def get_text_from_website(url):
+    links = []
     html = urlopen(Request(url, headers={'User-Agent': 'Mozilla/5.0'})).read()
     soup = BeautifulSoup(html, 'html.parser')
-    return "".join(soup.get_text())
+    for line in soup.find_all('a'):
+        links.append(line.get('href'))
+    return len(links)
 
 
 if __name__ == '__main__':
-    x = re.sub(' +', ' ', get_text_from_website('https://www.warbyparker.com/jobs'))
-    # time.sleep(3)
-    y = re.sub(' +', ' ', get_text_from_website('https://www.warbyparker.com/jobs'))
-    pprint(x)
-
+    x = get_text_from_website('https://www.datadoghq.com/jobs-engineering/')
+    time.sleep(1)
+    y = get_text_from_website('https://www.datadoghq.com/jobs-engineering/')
+    print(x, y)
+    print(x == y)
